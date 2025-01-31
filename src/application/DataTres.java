52b1e8dbac4c073b1845332b9dd4b28e03d349d4
GabePreare <gabriel.estudanteADS@gmail.com>
2025-01-31 17:28:24 -0300

    Pegando informações específicas com métodos acessores

--- src/application/DataTres.java
+++ src/application/DataTres.java
@@ -8,7 +8,7 @@ import java.time.ZoneId;
 public class DataTres {
 
 	public static void main(String[] args) {
-		LocalDate dataLocal = LocalDate.parse("2025-04-26");
+		LocalDate dataLocal = LocalDate.parse("2017-08-14");
 		LocalDateTime dataPrecisa = LocalDateTime.parse("2026-03-07T12:00:26");
 		Instant dataIng = Instant.parse("2027-05-25T15:30:24Z");
 		
@@ -17,6 +17,9 @@ public class DataTres {
 		
 		System.out.println(dataIng+"\n"+d1);
 		System.out.println("\n"+dataIng+"\n"+d2);
+		System.out.println("\nDia da dataLocal: " + dataLocal.getDayOfWeek());
+		System.out.println("Mês da dataLocal: " + dataLocal.getMonth());
+		System.out.println("Ano da dataLocal: " + dataLocal.getYear());
 	}
 
 }
